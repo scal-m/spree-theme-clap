9ccf2b8c8480ca2858860aa5f4fdc4dcb619b632
Pascal Mercier <scapoual@gmail.com>
2010-10-07 14:36:31 +0200

    fixed layout and new accordion based on jquery

---
+++ public/javascripts/elementsInitializer.js
@@ -0,0 +1,35 @@
+jQuery(document).ready(function() {
+	if($('#taxonomies')) {
+		$('#taxonomies').tabs('#taxonomies div', {tabs: 'h3', effect: 'horizontal'});
+	}
+	if($('.info')) {
+		resizeFloatingElementWidth($('.info'), $('#content'), 100 + 18);
+	}
+//	if($('#content')) {
+//		resizeFloatingElementHeight($('#content'), $(window), $('#header').height() + $('#footer').height());
+//	}
+});
+
+// with timer for IE support
+var resizeTimer = null;
+$(window).resize(function() {
+	if(resizeTimer) clearTimeout(resizeTimer); 
+	resizeTimer = setTimeout("if($('.info')) resizeFloatingElementWidth($('.info'), $('#content'), 100 + 18);", 100);
+});
+
+// without timer for more fluidity
+//$(window).resize(function() {
+//	if($('.info')) {
+//		resizeTimer = setTimeout("resizeFloatingElementWidth($('.info'), $('#content'), 100 + 18);", 100);
+//	}
+//});
+
+
+
+function resizeFloatingElementWidth(floatingElt, container, fixedSizeElt) {
+	floatingElt.width( container.width() - fixedSizeElt );
+}
+
+function resizeFloatingElementHeight(floatingElt, container, fixedSizeElt) {
+	floatingElt.height( container.height() - fixedSizeElt );
+}
